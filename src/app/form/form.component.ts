a81471bf567f85294f40826ffddfadd45c81c9b2
JuanVicenteVazquezG <juanvicentevazquezg@gmail.com>
2020-10-20 06:55:19 +0200

    viewChild Decorator example

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, ElementRef, ViewChild } from '@angular/core';
 import { Person } from '../class/person.model';
 
 @Component({
@@ -10,19 +10,25 @@ export class FormComponent {
   // public name: string;
   // public surname: string;
   @Output() createdPerson: EventEmitter<Person>;
+  @ViewChild('name') name: ElementRef;
+  @ViewChild('surname') surname: ElementRef;
 
   constructor() {
     this.createdPerson = new EventEmitter<Person>();
   }
 
-  onAddPerson(name: HTMLInputElement, surname: HTMLInputElement): void {
-    if (name.value.trim() === '') { return; }
-    if (surname.value.trim() === '') { return; }
-    const person: Person = new Person({ name: name.value, surname: surname.value });
+  onAddPerson(): void {
+    if (this.name.nativeElement.value === '') { return; }
+    if (this.surname.nativeElement.value === '') { return; }
+    const person: Person = new Person(
+      {
+        name: this.name.nativeElement.value,
+        surname: this.surname.nativeElement.value
+      });
     console.log(name);
     this.createdPerson.emit(person);
-    name.value = '';
-    surname.value = '';
+    this.name.nativeElement.value = '';
+    this.surname.nativeElement.value = '';
   }
 
 }
