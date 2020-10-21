f019125c45393e18f8660ab8138efc04d52aaf00
JuanVicenteVazquezG <juanvicentevazquezg@gmail.com>
2020-10-21 13:15:35 +0200

    inject one service into another service

--- src/app/class/persons.service.ts
+++ src/app/class/persons.service.ts
@@ -1,17 +1,20 @@
+import { Injectable } from '@angular/core';
 import { Person } from '../class/person.model';
-
+import { LogginService } from './LogginService.service';
+@Injectable()
 export class PersonsServices {
   public persons: Array<Person>;
 
-  constructor() {
+  constructor(private logginService: LogginService) {
     this.persons = [
       new Person({ name: 'Juan', surname: 'Vazquez Lopez' }),
       new Person({ name: 'Laura', surname: 'Perez' })
     ];
   }
   addPerson(person: Person): void {
+    this.logginService.sendMessageToConsole(`Added a person ${person.human.name}`);
     this.persons.push(person);
-    console.log(this.persons);
+
   }
 
 }
